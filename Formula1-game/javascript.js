12d970a288962deb4398d82958533156afe7b66f
Alessiobettens <alessio.bettens@student.ehb.be>
2026-05-04 00:16:08 +0200

    Game timer (30sec)

--- Formula1-game/javascript.js
+++ Formula1-game/javascript.js
@@ -25,3 +25,30 @@ function startCountdown() {
     }
   }, 1000);
 }
+
+
+// ===== GAME TIMER (30 sec) =====
+let gameTime = 30;
+let gameTimerInterval = null;
+let score = 0;
+
+function startGame() {
+  gameState = "playing";
+  gameTime = 30;
+  score = 0;
+
+  updateScore();
+
+  const timeEl = document.getElementById("time");
+  timeEl.innerText = "Time left: 30s";
+
+  gameTimerInterval = setInterval(() => {
+    gameTime--;
+    timeEl.innerText = "Time left: " + gameTime + "s";
+
+    if (gameTime <= 0) {
+      clearInterval(gameTimerInterval);
+      endGame();
+    }
+  }, 1000);
+}
